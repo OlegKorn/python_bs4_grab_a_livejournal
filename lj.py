3468c7d7dcd6bd14814f63f41ecbedd80e743ecd
O <48214249+OlegKorn@users.noreply.github.com>
2020-09-17 15:52:21 +0300

    Update lj.py

--- lj.py
+++ lj.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 import re, os
 
 
-URL = 'https://historical-fact.livejournal.com/calendar'
+URL = 'https://germanych.livejournal.com/calendar'
 this_year = URL.replace('calendar', '2020')
 
 LJ_NAME = re.search('://(.*).live', URL).group(1)
@@ -12,6 +12,7 @@ home = 'G:/Desktop/py/lj/' + LJ_NAME + '/'
 lj_text = home + LJ_NAME + '.txt'
 links_by_month = home + LJ_NAME + '_get_posts_by_month.txt'
 all_posts = home + LJ_NAME + '_all_posts.txt'
+
 
 
 class LJ:
@@ -37,7 +38,6 @@ class LJ:
 
 
     def get_years(self):
-        
         soup = self.get_soup()
 
         years = [_['href'] for _ in soup.find('ul', class_='year').find_all('a')]
@@ -111,7 +111,106 @@ class LJ:
 
 
 
-lj = LJ()
-lj.get_all_posts()
+
+
+
+
+
+
+
+class LJ2:
+
+    def __init__(self):
+        if not os.path.exists(home):
+            os.mkdir(home, mode=0o777)
+
+
+    def get_soup(self, url=URL):
+        try: 
+            session = requests.Session()
+            request = session.get(url)
+            soup = bs(request.content, 'html.parser')
+
+            return soup
+
+        except Exception as e:
+            print(e)
+            pass
+
+
+    def get_years(self):
+        soup = self.get_soup()
+
+        years = [_['href'] for _ in soup.find('ul', class_='j-years-nav').find_all('a')]
+        # append 2020 year
+        years.append(this_year)
+
+        return years
+            
+
+    def get_posts_by_month(self):
+        years = self.get_years()
+
+        links_by_month_ = open(links_by_month, 'w')
+
+        for year in years:
+            soup = self.get_soup(year)
+            for month in soup.find_all('div', class_='j-calendar-month'):
+                _ = month.a['href']
+                links_by_month_.write(_ + '\n')
+
+        links_by_month_.close()
+
+
+    def get_all_posts(self):
+        months_ = open(links_by_month, 'r').readlines()
+        posts_ = open(all_posts, 'w')
+ 
+        for month in months_:
+            soup = self.get_soup(month.strip())
+
+            for post in soup.find_all('a', class_='j-day-subject-link'):
+                print(post['href'])
+                posts_.write(post['href'] + '\n')
+
+        posts_.close()
+
+
+    def save_lj(self):
+
+        all_posts_ = open(all_posts, 'r').readlines()
+        lj_ = open(lj_text, 'w', encoding='utf-8')
+ 
+        for _ in all_posts_:
+            soup = self.get_soup(_.strip())
+
+            try:
+                title = soup.find('h1', class_='b-singlepost-title').text.strip()
+            except AttributeError:
+                title = 'No subject'
+            
+            link = _.strip()
+            text = soup.find('article', class_='b-singlepost-body').prettify()
+            data = re.sub(r'<.*?>', '', text).strip()
+
+            lj_.write('=====================================================================' + '\n')
+            lj_.write('=====================================================================' + '\n')
+            lj_.write(title + '\n')
+            lj_.write(link + '\n')
+            lj_.write(data + '\n')
+            lj_.write('=====================================================================' + '\n')
+            lj_.write('=====================================================================' + '\n')
+            
+            print(link)
+            
+        lj_.close()
+
+
+
+
+lj = LJ2()
+# lj.get_posts_by_month()
+# lj.get_all_posts()
 lj.save_lj()
+
 
